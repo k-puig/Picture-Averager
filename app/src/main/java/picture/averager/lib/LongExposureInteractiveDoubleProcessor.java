c5f7756a668646edc04e4ad0aff66967f9d79db7
Kayla Puig <119972216+KaylaPuig@users.noreply.github.com>
2023-06-10 21:31:32 -0400

    Added some more experimental stuff to the interactive processor class

--- app/src/main/java/picture/averager/lib/LongExposureInteractiveDoubleProcessor.java
+++ app/src/main/java/picture/averager/lib/LongExposureInteractiveDoubleProcessor.java
@@ -9,7 +9,6 @@ public class LongExposureInteractiveDoubleProcessor extends PictureProcessor {
     private double[][] g_data;
     private double[][] b_data;
     private double brightnessCoefficient = 1.0;
-    private double brightnessExponent = 1.0;
     private Scanner scanner;
 
     public LongExposureInteractiveDoubleProcessor(int rows, int cols)
@@ -70,9 +69,9 @@ public class LongExposureInteractiveDoubleProcessor extends PictureProcessor {
             for (int c = 0; c < Math.min(img.getWidth(), this.cols); c++)
             {
                 int argb = img.getRGB(c, r);
-                double red = Math.pow((double) ((argb & 0x00FF0000) >> 16), brightnessExponent);
-                double green = Math.pow((double) ((argb & 0x0000FF00) >> 8), brightnessExponent);
-                double blue = Math.pow((double) (argb & 0x000000FF), brightnessExponent);
+                double red = expMap((double) ((argb & 0x00FF0000) >> 16), 1275.0, 100.0);
+                double green = expMap((double) ((argb & 0x0000FF00) >> 8), 1275.0, 100.0);
+                double blue = expMap((double) (argb & 0x000000FF), 1275.0, 100.0);
                 r_data[r][c] = (imageCount * r_data[r][c] + red) / (imageCount + 1);
                 g_data[r][c] = (imageCount * g_data[r][c] + green) / (imageCount + 1);
                 b_data[r][c] = (imageCount * b_data[r][c] + blue) / (imageCount + 1);
@@ -85,7 +84,7 @@ public class LongExposureInteractiveDoubleProcessor extends PictureProcessor {
     @Override
     public boolean hasImages() 
     {
-        System.out.println("Enter a positive decimal to change the brightness coefficient (1.0 default) of the output image, or E to exit. ");
+        System.out.println("Enter a positive decimal to change the brightness coefficient (1.0 default) of the output image, or E to exit, or P to print out the raw double data. ");
         String input = scanner.nextLine();
 
         if (input.toLowerCase().equals("e"))
@@ -93,11 +92,34 @@ public class LongExposureInteractiveDoubleProcessor extends PictureProcessor {
             scanner.close();
             return false;
         }
+        else if (input.toLowerCase().equals("p"))
+        {
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    System.out.printf("{%f, %f, %f} ", r_data[r][c], g_data[r][c], b_data[r][c]);
+                }
+                System.out.println();
+            }
+        }
         else
         {
             brightnessCoefficient = Double.parseDouble(input);
         }
 
         return true;
+    }
+
+    /*
+     * expMap(x) = ae^((ln((u+a)/(a)))/255)x)-a
+     * Maps an input value x onto the range [0, upper] using an exponential function
+     */
+    private static double expMap(double x, double upper, double curveModifier)
+    {
+        double expCoefficient = Math.log((upper + curveModifier) / curveModifier) / 255.0;
+        double exponentialResult = Math.exp(expCoefficient * x);
+        double finalAnswer = curveModifier * exponentialResult - curveModifier;
+        return finalAnswer;
     }
 }
